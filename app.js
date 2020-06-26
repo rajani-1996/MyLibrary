d5bcd6693e61efae9a9260ff3c5b485b32b0c5cf
rajani-1996 <rajanirussal@gmail.com>
2020-06-26 11:35:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const app=new express;
+const chalk=require('chalk');
+const path=require('path');
+const nav=require('./src/routes/nav');
+const bodyparser=require('body-parser');
+
+const authorRouter=require("./src/routes/authorrouter");
+const bookRouter=require("./src/routes/bookRoutes");
+const loginRouter=require("./src/routes/loginrouter");
+const registerRouter=require("./src/routes/registerrouter")
+
+app.set('views','./src/views');
+app.set('view engine','ejs');
+app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public/images'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+;
+
+app.use("/authors",authorRouter);
+app.use("/login",loginRouter);
+app.use("/books",bookRouter);
+app.use("/register",registerRouter);
+
+
+
+app.get('/',function(req,res){
+res.render("index",{
+    nav,
+    title:"E-Library"
+});
+});
+app.get('/about',function(req,res){
+    res.render("about",{
+        nav,
+        title:"about"
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("Server listening to : 3000");
+});
---
+++ src/routes/nav.js
@@ -0,0 +1,43 @@
+const nav=[
+// {
+//         "link":"/",
+//     "title":"Home"
+// },
+{
+    "link":"/index",
+// "title":"E-BookReader"
+},
+
+{
+    "link":"/books",
+"title":"Books"
+},
+{
+    "link":"/authors",
+"title":"Authors"
+},
+{
+    "link":"/books/admin",
+"title":"AddBook"
+},
+
+{
+    "link":"/authors/admin",
+"title":"AddAuthor"
+},
+{"link":"/about",
+"title":"About"},
+// {
+//     "link":"/login",
+// "title":"Login"
+// },
+{
+    "link":"/register",
+"title":"Register"
+},
+{
+    "link":"/",
+"title":"Logout"
+},
+];
+module.exports = nav;
